c104f9ffd19e6cdadebe63d3b551c348f33de6e4
tarunjoshi16 <tarun.j@selectiva.com>
2021-10-20 13:29:03 +0530

    resume full folder from starting

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            $('section').each(function(i) {
+                // The number at the end of the next line is how pany pixels you from the top you want it to activate.
+                if ($(this).position().top <= windscroll - 0) {
+                    $('nav li.active').removeClass('active');
+                    $('nav li').eq(i).addClass('active');
+                }
+            });
+
+        } else {
+
+            $('nav li.active').removeClass('active');
+            $('nav li:first').addClass('active');
+        }
+
+    }).scroll();
+
+    var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveBackground() {
+  x += (lFollowX - x) * friction;
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+  $('.bg').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveBackground);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+  lFollowY = (10 * lMouseY) / 100;
+
+});
+
+moveBackground();
+
+});
+
+
