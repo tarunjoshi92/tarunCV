2e0d4ebd50209127eae745a4c62343c65d0601a1
tarunjoshi92 <tarunjoshi92@gmail.com>
2021-10-20 18:09:07 +0530

    performance improve

--- js/custom.js
+++ js/custom.js
@@ -19,37 +19,35 @@ $(document).ready(function() {
     }).scroll();
 
     var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
+        lFollowY = 0,
+        x = 0,
+        y = 0,
+        friction = 1 / 30;
 
-function moveBackground() {
-  x += (lFollowX - x) * friction;
-  y += (lFollowY - y) * friction;
-  
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+    function moveBackground() {
+        x += (lFollowX - x) * friction;
+        y += (lFollowY - y) * friction;
 
-  $('.bg').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-  });
+        translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
 
-  window.requestAnimationFrame(moveBackground);
-}
+        $('.bg').css({
+            '-webit-transform': translate,
+            '-moz-transform': translate,
+            'transform': translate
+        });
 
-$(window).on('mousemove click', function(e) {
+        window.requestAnimationFrame(moveBackground);
+    }
 
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-  lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-  lFollowY = (10 * lMouseY) / 100;
+    $(window).on('mousemove click', function(e) {
+
+        var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+        var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+        lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+        lFollowY = (10 * lMouseY) / 100;
+
+    });
+
+    moveBackground();
 
 });
-
-moveBackground();
-
-});
-
-
